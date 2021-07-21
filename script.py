fc17c64c889867fb670baf8e8fc80a09007062e9
Winbert Zhang <winbert@x-camp.academy>
2021-07-20 18:32:59 -0700

    Initial commits
    yes

--- script.py
+++ script.py
@@ -1,10 +1,19 @@
 # in command prompt, type "pip install pynput" to install pynput.
 from pynput.keyboard import Key, Controller
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 import time
 
-keyboard = Controller()
-key = Key.left
+driver = webdriver.Chrome()
+driver.get('https://isthisprime.com/game/')
+assert "Python" in driver.title
+elem = driver.find_element_by_id("n")
+while(True):
+    elem.send_keys(Keys.LEFT)
 
-time.sleep(2)
-keyboard.press(key)
-keyboard.release(key)
+time.sleep(1000)
+
+#time.sleep(2)
+#keyboard.press(key)
+#keyboard.release(key)
